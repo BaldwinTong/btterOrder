20a42fd64f85d7b7ff15c5bdb567da392ca212b3
BaldwinTong <765160998@qq.com>
2021-09-07 17:20:39 +0800

    layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
-
 const Login = () =>
     import ( /* webpackChunkName: "about" */ '../views/Login.vue')
+const routes = [{
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Home.vue')
+    }, {
+        path: '/404',
+        name: 'notFound',
+        component: () =>
+            import ('../views/404.vue')
+    }, {
+        path: "*",
+        redirect: "/404"
+    }
+]
 
 const router = new VueRouter({
-    routes: [{
-            path: '/',
-            redirect: '/home'
-        },
-        {
-            path: '/login',
-            name: 'Login',
-            component: Login
-        },
-        {
-            path: '/home',
-            name: 'Home',
-            component: () =>
-                import ( /* webpackChunkName: "about" */ '../views/Home.vue')
-        }, {
-            path: '/404',
-            name: 'notFound',
-            component: () =>
-                import ('../views/404.vue')
-        }, {
-            path: "*",
-            redirect: "/404"
-        }
-    ]
+    routes
 })
 
-
+// 解决 Vue 重复点击相同路由
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+    if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+    return originalPush.call(this, location).catch(err => err)
+}
 
 // router.beforeEach((to, from, next) => {
 //     if (to.path === '/login') return next();
